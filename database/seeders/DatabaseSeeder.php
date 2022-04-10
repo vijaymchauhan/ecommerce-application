ed44b71cc44a3ba1e96c4a1097f4301d742851b9
vijay chauhan <vijay.chauhan@goodworklabs.com>
2022-04-10 19:00:17 +0530

    attribute

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(SettingsTableSeeder::class);
 
         $this->call(CategoriesTableSeeder::class);
-        
+        $this->call(AttributesTableSeeder::class);
+
     }
 }
